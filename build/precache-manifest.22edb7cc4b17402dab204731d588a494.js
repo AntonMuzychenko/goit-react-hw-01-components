3270453e6f9ea758051ac062a60a3e6ea1996d87
AntonMuzychenko <49153700+AntonMuzychenko@users.noreply.github.com>
2019-07-16 19:04:33 +0300

    deploy done. create homepage. add all components. create css file

---
+++ build/precache-manifest.22edb7cc4b17402dab204731d588a494.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0e5e1b0f858a9ea543bef7e14e0df05",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "8af6a65b001b2952e01a",
+    "url": "/goit-react-hw-01-components/static/css/main.7321168e.chunk.css"
+  },
+  {
+    "revision": "af675272ae8bec68157f",
+    "url": "/goit-react-hw-01-components/static/js/2.8332e7eb.chunk.js"
+  },
+  {
+    "revision": "8af6a65b001b2952e01a",
+    "url": "/goit-react-hw-01-components/static/js/main.3f8bcc95.chunk.js"
+  },
+  {
+    "revision": "b0f8f2e436fc577230d9",
+    "url": "/goit-react-hw-01-components/static/js/runtime~main.681d89f7.js"
+  }
+]);
