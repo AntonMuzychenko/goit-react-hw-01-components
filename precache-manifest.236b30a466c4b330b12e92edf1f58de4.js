86b49ba952c88c4b51fdfc8c5f5214de6c3cbf44
AntonMuzychenko <49153700+AntonMuzychenko@users.noreply.github.com>
2019-07-18 12:39:22 +0300

    Updates

---
+++ precache-manifest.236b30a466c4b330b12e92edf1f58de4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a75ef8b307a20b8a4dad716a908b5e0f",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "72a15c18e20bb8c7c357",
+    "url": "/goit-react-hw-01-components/static/css/main.574090eb.chunk.css"
+  },
+  {
+    "revision": "c31b5ecc3c8d85e17e7d",
+    "url": "/goit-react-hw-01-components/static/js/2.44e0a5b2.chunk.js"
+  },
+  {
+    "revision": "72a15c18e20bb8c7c357",
+    "url": "/goit-react-hw-01-components/static/js/main.faebf8de.chunk.js"
+  },
+  {
+    "revision": "b0f8f2e436fc577230d9",
+    "url": "/goit-react-hw-01-components/static/js/runtime~main.681d89f7.js"
+  }
+]);
